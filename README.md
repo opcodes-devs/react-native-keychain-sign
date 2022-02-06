a8df57cb49ae85f193ed9995ef27ad4bf137c3d9
vtolochk <vtolochk@gmail.com>
2022-02-06 10:54:54 +0200

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# react-native-keychain-sign
+
+keychain signer
+
+## Installation
+
+```sh
+npm install react-native-keychain-sign
+```
+
+## Usage
+
+```js
+import { multiply } from "react-native-keychain-sign";
+
+// ...
+
+const result = await multiply(3, 7);
+```
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
---
+++ android/src/main/java/com/reactnativekeychainsign/KeychainSignModule.java
@@ -0,0 +1,34 @@
+package com.reactnativekeychainsign;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = KeychainSignModule.NAME)
+public class KeychainSignModule extends ReactContextBaseJavaModule {
+    public static final String NAME = "KeychainSign";
+
+    public KeychainSignModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    @NonNull
+    public String getName() {
+        return NAME;
+    }
+
+
+    // Example method
+    // See https://reactnative.dev/docs/native-modules-android
+    @ReactMethod
+    public void multiply(int a, int b, Promise promise) {
+        promise.resolve(a * b);
+    }
+
+    public static native int nativeMultiply(int a, int b);
+}
